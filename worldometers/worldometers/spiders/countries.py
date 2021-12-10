0e6e3c536c04e4c6b8952f6c4386e489eb6e6556
Surya Bhusal <suryabhusal11@gmail.com>
2021-12-10 18:57:09 +0545

    Countries population by year spider.

--- worldometers/worldometers/spiders/countries.py
+++ worldometers/worldometers/spiders/countries.py
@@ -22,19 +22,20 @@ class CountriesSpider(scrapy.Spider):
 
             """
                 # make sure allowed_domains has no slash '/'
-                yield response.follow(url=link)
+                yield response.follow(url=link, callback=self.parse_country)
             """
-            yield scrapy.Request(url=absolute_url, callback=self.parse_country)
+            yield scrapy.Request(url=absolute_url, callback=self.parse_country, meta={'country_name': name})
 
     def parse_country(self, response):
         """Crawl country page"""
+        name = response.request.meta['country_name']
         rows = response.xpath('(//table[@class="table table-striped table-bordered table-hover table-condensed table-list"])[1]/tbody/tr')
-
         for row in rows:
             year = row.xpath(".//td[1]/text()").get()
             population = row.xpath(".//td[2]/strong/text()").get()
 
             yield {
+                'country_name': name,
                 'year': year,
                 'population': population
             }
