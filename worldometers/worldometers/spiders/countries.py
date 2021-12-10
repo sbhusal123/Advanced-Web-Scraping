573cf910484aae856b56e506d3e9d9ca81123de2
Surya Bhusal <suryabhusal11@gmail.com>
2021-12-10 18:11:55 +0545

    Scraping Countries population by year.

--- worldometers/worldometers/spiders/countries.py
+++ worldometers/worldometers/spiders/countries.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 import scrapy
-
+import logging
 
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
 
     # all the domains this spider is allowed to access, no need to add protocol
-    allowed_domains = ['www.worldometers.info/']
+    allowed_domains = ['www.worldometers.info']
 
     # starts crawling the site from url below.
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
@@ -17,8 +16,25 @@ class CountriesSpider(scrapy.Spider):
         for country in countries:
             name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
-            yield {
-                "country_name": name,
-                "country_link": link
-            }
 
+            # Gives full url with protocal: https://xyz.com/path/to/...
+            absolute_url = response.urljoin(link)
+
+            """
+                # make sure allowed_domains has no slash '/'
+                yield response.follow(url=link)
+            """
+            yield scrapy.Request(url=absolute_url, callback=self.parse_country)
+
+    def parse_country(self, response):
+        """Crawl country page"""
+        rows = response.xpath('(//table[@class="table table-striped table-bordered table-hover table-condensed table-list"])[1]/tbody/tr')
+
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
+
+            yield {
+                'year': year,
+                'population': population
+            }
